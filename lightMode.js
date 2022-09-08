3bb5afda1cd30c75a4d8eb5e6efdc5b31b21a2cb
Emilie Ickart <emi.ickart@gmail.com>
2022-09-08 12:04:46 +0200

    Refactor Javascript code

--- lightMode.js
+++ lightMode.js
@@ -1,26 +1,28 @@
 let mode = 'light';
+let color, image, label, label2;
 
 function selectMode(mode) {
-    let color = 'ghostWhite';
-    let image = 'img/ninja-index.jpg';
-    let label = 'Index Mode';
-    let label2 = 'Select your favorite mode for background';
-
-    if (mode === 'dark') {
-        color = 'rgb(42, 41, 48)';
-        image = 'img/ninja-darkmode.png';
-        label = 'Dark Ninja Mode';
-        label2 = '';
-    } else if (mode === 'light') {
-        color = 'ghostWhite';
-        image = 'img/ninja-lightmode.png';
-        label = 'Light Ninja Mode';
-        label2 = '';
-    } else {
-        color = 'dimGray';
-        image = 'img/ninja-greymode.png';
-        label = 'Grey Ninja Mode';
-        label2 = '';
+    switch (mode) {
+        case 'dark':
+            color = 'rgb(42, 41, 48)';
+            image = 'img/ninja-darkmode.png';
+            label = 'Dark Ninja Mode';
+            label2 = '';
+            break;
+        case 'light':
+            color = 'ghostWhite';
+            image = 'img/ninja-lightmode.png';
+            label = 'Light Ninja Mode';
+            label2 = '';
+            break;
+        case 'grey':
+            color = 'dimGray';
+            image = 'img/ninja-greymode.png';
+            label = 'Grey Ninja Mode';
+            label2 = '';
+            break;
+        // default:
+        //     break;
     }
 
     document.getElementById('icon').src = image;
